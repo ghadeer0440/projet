8ee783e3f8026d66b0d01023e99b22412b62f646
ghadeer0440 <ghadeer.a.alrakaf@gmail.com>
2023-10-05 12:01:21 +0300

    Initial Commit

---
+++ projet/projetApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetApp.swift
+//  projet
+//
+//  Created by Ghadeer on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct projetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
